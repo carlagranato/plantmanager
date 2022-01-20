baf2166c8ad7947fd016a2b55d5a2d90e6c856b0
carlagranato <gnt.carla@gmail.com>
2022-01-19 22:45:13 -0300

    Estilização tela PlantSave

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Alert, StyleSheet, View, Text, Image, ScrollView, Platform, TouchableOpacity } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { SvgFromUri } from 'react-native-svg';
 import waterdrop from '../assets/waterdrop.png'
 import { Button } from '../components/button';
@@ -58,23 +59,71 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'space-between',
-        backgroundColor: colors.shape,
+        backgroundColor: colors.shape
 
     },
 
-    plantName: {
-
-    },
-
-    plantAbout: {
-
+    plantInfo: {
+        flex: 1,
+        paddingHorizontal: 30,
+        paddingVertical: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: colors.shape
     },
 
     controller: {
+        backgroundColor: colors.white,
+        paddingHorizontal: 20,
+        paddingTop: 20,
+        paddingBottom: getBottomSpace() || 20 //o getBottomSpace pega o espaço da tela do iPhone, mas caso seja um Android ele usa 20pixels
+    },
 
+    plantName: {
+        fontFamily: fonts.heading,
+        fontSize: 24,
+        color: colors.heading,
+        marginTop: 15,
+    },
+
+    plantAbout: {
+        textAlign: 'center',
+        fontFamily: fonts.text,
+        color: colors.heading,
+        fontSize: 17,
+        marginTop: 10
     },
 
     tipContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: colors.blue_light,
+        padding: 20,
+        borderRadius: 20,
+        position: 'relative',
+        bottom: 60 //Conta de baixo pra cima, qual será a posição do componente (já que informamos acima que é relativa)
+    },
 
+    tipImage: {
+        height: 56,
+        width: 56
+    },
+
+    tipText: {
+        flex:1,
+        marginLeft: 20,
+        fontFamily: fonts.text,
+        color: colors.blue,
+        fontSize: 17,
+        textAlign: 'justify'
+    },
+
+    alertLabel: {
+        textAlign: 'center',
+        fontFamily: fonts.complement,
+        color: colors.heading,
+        fontSize: 12,
+        marginBottom: 5
     }
 })
